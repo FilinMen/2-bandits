4985cbc6572ba3e667b0f3f3086a3636c9d536d9
FilinMen <fiilinkadabra@tutamail.com>
2024-10-04 20:47:20 +0300

    good pleas

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 input_data = open("input.txt","r")
 data = input_data.read()
 data = data.split()
-a = int(data[0])
-b = int(data[1])
-d = 10 - a
-r = 10 - b
-t = str(d) + " " + str(r)
 output_data = open("output.txt","w")
-output_data.write(t)
+output_data.write( str(10 - int(data[0])) + " " + str(10 - int(data[1])))
 input_data.close()
