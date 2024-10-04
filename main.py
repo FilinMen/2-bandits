484cd04c4f4c8d2c25c8691b4ac8576d4f752043
FilinMen <fiilinkadabra@tutamail.com>
2024-10-04 20:32:11 +0300

    good

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 input_data = open("input.txt","r")
 data = input_data.read()
-
+data = data.split()
 a = int(data[0])
 b = int(data[1])
-d = 10
-
-c = d - a # первый стрелок не прострелил
-r = d - b #второй стрелок не прострелил
-
-t = str(c) + str(r)
+d = 10 - a
+r = 10 - b
+t = str(d) + " " + str(r)
 output_data = open("output.txt","w")
 output_data.write(t)
-output_data.close()
 input_data.close()
